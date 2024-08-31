24d7135e7a0d26c8b7095b39462fe112bbcaa4e8
NoahGX <noahjgarcia2019@gmail.com>
2024-08-31 03:34:16 -0700

    Update

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
 import requests
 
-# Read the API Key from a text file
-API_KEY = open("../data/api.txt", "r").read()
+# Define main function
+def main():
+    # Read the API Key from a text file
+    API_KEY = open("../data/api.txt", "r").read()
  
-# Prompt the user to enter a city name of their choosing
-user_input = input("Enter City Name: ")
+    # Prompt the user to enter a city name of their choosing
+    user_input = input("Enter City Name: ")
+    
+    # Make an API request to get the URL
+    weather_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q={user_input}&units=imperial").json()
+    
+    # Print the results
+    if weather_data['cod'] == "404":
+        print("Invalid City Name.")
+    else:
+        print(weather_data)
 
-# Make an API request to get the URL
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q={user_input}&units=imperial").json()
-
-# Print the results
-if weather_data['cod'] == "404":
-    print("Invalid City Name.")
-else:
-    print(weather_data)
+# Main function
+if __name__ == "__main__":
+    main()
