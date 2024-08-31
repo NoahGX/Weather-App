836a3e41578d32d22ef626498d2a9c5f9f299f95
NoahGX <noahjgarcia2019@gmail.com>
2024-08-31 02:58:13 -0700

    Update

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ API_KEY = open("../data/api.txt", "r").read()
 # Prompt the user to enter a city name of their choosing
 user_input = input("Enter City Name: ")
 
-# Make an API request to get the URL as in JSON format
+# Make an API request to get the URL
 weather_data = requests.get(
     f"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q={user_input}")
 
