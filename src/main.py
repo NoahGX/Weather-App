a8cae68de5ea2ea5ee4ce0abf15eade23a4cc6da
NoahGX <noahjgarcia2019@gmail.com>
2024-09-01 20:25:01 -0700

    Update

--- src/main.py
+++ src/main.py
@@ -7,23 +7,23 @@ import datetime as dt
 # Initialize console
 console = Console()
 
-# Define function to fetch data for weather information
+# Define function to fetch weather data from the API
 def fetch_weather(API_KEY, user_input):
-    # Fetch URL to make an API request for current data
+    # Fetch URL to make an API request
     URL = f"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q={user_input}&units=imperial"
 
-    # Make an HTTP request to the URL to recieve data
+    # Make an HTTP request to the API get the data
     data_response = requests.get(URL)
     
     # Parse the JSON response
     data_response = data_response.json()
 
-    # Return the data response
+    # Return the data
     return data_response
 
 # Define function to display the weather data
 def display_weather(weather_data):
-    # Define the date and time
+    # Calculate the local date and time based on timezone
     dateTime = dt.datetime.fromtimestamp(weather_data['dt'], dt.timezone.utc)
     dateTime = dateTime + dt.timedelta(seconds=weather_data['timezone'])
 
@@ -31,7 +31,7 @@ def display_weather(weather_data):
     date = dateTime.strftime("%A, %b %-d %Y")
     time = dateTime.strftime("%I:%M:%S %p")
 
-    # Assign variables for cleaner code
+    # Extract details from the weather data
     city = weather_data['name']
     weather = weather_data['weather'][0]['main']
     description = weather_data['weather'][0]['description']
@@ -42,9 +42,10 @@ def display_weather(weather_data):
     visibility = weather_data['visibility']
     wind_speed = weather_data['wind']['speed']
 
-    # If there are no errors, print data
-    console.print(f"[bold]{city}[/bold] Local Date: [bold green]{date}[/bold green]")
-    console.print(f"[bold]{city}[/bold] Local Time: [bold green]{time}[/bold green]")
+    # Print the formatted weather data
+    console.print(f"Fetching and Displaying Data...", style="bold green")
+    console.print(f"[bold]{city}[/bold] Local Date: [bold magenta]{date}[/bold magenta]")
+    console.print(f"[bold]{city}[/bold] Local Time: [bold magenta]{time}[/bold magenta]")
     console.print(f"\tCurrent Weather: "f"[bold cyan]{weather}[/bold cyan]")
     console.print(f"\tDescription: [bold cyan]{description}[/bold cyan]")
     console.print(f"\tTemperature: {temperature} ºF")
@@ -60,29 +61,36 @@ def main():
         # Read the API Key from a text file
         with open("../data/api.txt", "r") as file:
             API_KEY = file.read()
-
-        # Prompt the user to enter a city name of their choosing
-        user_input = input("Enter City Name: ")
-        
-        # Call function to make an API request for weather data
-        weather_data = fetch_weather(API_KEY, user_input)
-
-        # Error Handling
-        if weather_data['cod'] == '404':
-            raise ValueError(weather_data['message'])
-
     except FileNotFoundError as file_err:
+        # Return an error if the file does not exist
         console.print(f"File Error: {file_err}", style="bold red")
-    except ValueError as api_err:
-        console.print(f"API Error: {api_err}", style="bold red")
-    except requests.exceptions.RequestException as net_err:
-        console.print(f"Network Error: {net_err}", style="bold red")
-    except Exception as err:
-        console.print(f"Error: {err}", style="bold red")
+        return
+    
+    while True:
+        # Prompt the user to input a city name
+        user_input = input("Enter City Name (type 'exit' to quit): ")
+
+        if user_input.lower() == "exit":
+            console.print("Terminating Program... Goodbye!", style="bold green")
+            break
         
-    else:
-        # Call the function to display the weather data
-        display_weather(weather_data)
+        try:
+            # Fetch weather data for the given city using the API Key
+            weather_data = fetch_weather(API_KEY, user_input)
+            
+            # Handle Errors and Exceptions that may occur
+            if weather_data['cod'] == '404':
+                raise ValueError(weather_data['message'])
+        except ValueError as api_err:
+            console.print(f"API Error: {api_err}", style="bold red")
+        except requests.exceptions.RequestException as net_err:
+            console.print(f"Network Error: {net_err}", style="bold red")
+        except Exception as err:
+            console.print(f"Error: {err}", style="bold red")
+        
+        else:
+            # If there are no errors, display the weather data
+            display_weather(weather_data)
 
 # Main function
 if __name__ == "__main__":
